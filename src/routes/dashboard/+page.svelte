e4e5b31e1b67c9a3957d7ae4a4003612193eab4c
azkar-sh <akbar.dizir@gmail.com>
2023-12-08 17:45:01 +0700

    add: buttons and color code

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,72 @@
+<script>
+	let showMenu = false;
+	let taskMenu = false;
+	let inboxMenu = false;
+
+	function handleTaskMenu() {
+		taskMenu = !taskMenu;
+		inboxMenu = false;
+	}
+
+	function handleInboxMenu() {
+		inboxMenu = !inboxMenu;
+		taskMenu = false;
+	}
+
+	function handleQuickMenu() {
+		showMenu = !showMenu;
+		taskMenu = false;
+		inboxMenu = false;
+	}
+</script>
+
+<div class="flex flex-row w-screen h-screen bg-main-background overflow-hidden">
+	<div class="w-1/5 border-e"></div>
+	<div class="flex flex-col justify-between w-4/5">
+		<div class="flex">
+			<img src="/icons/magnifying.svg" alt="" class="p-3 bg-primary-dark-grey w-10" />
+			<input
+				type="text"
+				placeholder="Type here"
+				class="input w-full rounded-none bg-primary-dark-grey focus:outline-none focus:ring-0 text-white"
+			/>
+		</div>
+
+		<div class="self-end p-12 flex flex-row items-end gap-5">
+			<div class={`task-inbox ${showMenu ? 'visible' : 'hidden'}`}>
+				<button class="flex flex-col items-center gap-2 w-16" on:click={handleInboxMenu}>
+					<span class="text-white">Inbox</span>
+					<img src="/icons/btn-inbox.svg" alt="" />
+				</button>
+				<button class="flex flex-col items-center gap-2 w-16" on:click={handleTaskMenu}>
+					<span class="text-white">Task</span>
+					<img src="/icons/btn-task.svg" alt="" />
+				</button>
+			</div>
+
+			<button
+				class={`z-10 w-16 ${taskMenu || inboxMenu ? 'hidden' : ''}`}
+				on:click={handleQuickMenu}
+			>
+				<img src="/icons/btn-quick.svg" alt="" />
+			</button>
+		</div>
+	</div>
+</div>
+
+<style>
+	.task-inbox {
+		display: flex;
+		gap: 20px;
+		opacity: 0;
+		transform: translateX(100%);
+		transition:
+			opacity 0.3s ease-out,
+			transform 0.3s ease-out;
+	}
+
+	.task-inbox.visible {
+		opacity: 1;
+		transform: translateX(0);
+	}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				'main-background': '#333333',
+				'primary-blue': '#2f80ed',
+				'primary-dark-grey': '#4f4f4f',
+				'primary-light-grey': '#828282',
+				'primary-cream': '#e0e0e0',
+				'indicator-light-yellow': '#f8b76b',
+				'indicator-purple': '#8785ff',
+				'indicator-red': '#eb5757',
+				'indicator-yellow': '#f2c94c',
+				'chat-yellow': '#fceed3',
+				'chat-yellow-hover': '#e5a443',
+				'chat-purple': '#eedcff',
+				'chat-purple-hover': '#9b51e0',
+				'chat-green': '#d2f2ea',
+				'chat-green-hover': '#43B78D',
+				'sticker-grey': '#e9f3ff',
+				'sticker-orange': '#FDCFA4',
+				'sticker-light-orange': '#F9E9C3',
+				'sticker-blue': '#AFEBDB',
+				'sticker-green': '#CBF1C2',
+				'sticker-purple': '#CFCEF9',
+				'sticker-light-purple': '#F9E0FD'
+			}
+		}
+	},
+	daisyui: {
+		themes: [
+			{
+				mytheme: {}
+			}
+		]
+	},
+	plugins: [require('daisyui')]
+};
